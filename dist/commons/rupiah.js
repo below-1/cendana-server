1ac16bd26eaaabf3066adb0f85d252055a01abf5
jordan meta <jordanmeta@mail.com>
2021-11-09 23:39:35 +0800

    remove dist/ from cache

--- dist/commons/rupiah.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.rupiah = void 0;
-function rupiah(x) {
-    return x.toLocaleString('id');
-}
-exports.rupiah = rupiah;
